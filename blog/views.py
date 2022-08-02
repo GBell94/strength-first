a566cf23a1c0526cc2ee6b24cd096ca9a873a1f8
GBell94 <gmb1294@gmail.com>
2022-08-02 20:42:00 +0000

    Add html to index and post detail templates to display post list and individual posts

--- blog/views.py
+++ blog/views.py
@@ -7,9 +7,9 @@ from .models import Post
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
-    #paginate_by = 5
+    paginate_by = 5
 
 
 class PostDetail(generic.DetailView):
     model = Post
-    template_name = 'post_detail.html'
+    template_name = 'post-detail.html'

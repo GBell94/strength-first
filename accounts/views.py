1fafe2353b34b248d96ad731061e725642b22a0a
GBell94 <gmb1294@gmail.com>
2022-08-04 18:07:34 +0000

    Add login page

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,32 @@
-from django.shortcuts import render, redirect
-from .forms import RegisterForm
+#from django.shortcuts import render, redirect
+#from .forms import RegisterForm
 
 # Create your views here.
-def register(response):
-    if response.method == "POST":
-        form = RegisterForm(response.POST)
+#def register(response):
+    #if response.method == "POST":
+        #form = RegisterForm(response.POST)
+        #if form.is_valid():
+            #form.save()
+            
+        #return redirect('/home')  
+    #else:   
+        #form = RegisterForm()
+
+    #return render(response, "register.html", {"form": form})
+
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            
-        return redirect('/home')  
-    else:   
-        form = RegisterForm()
-
-    return render(response, "register.html", {"form": form})
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your account has been created! You can now log in')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'register.html', {'form': form})
--- strengthfirst/urls.py
+++ strengthfirst/urls.py
@@ -14,12 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
-from accounts import views as v
+from accounts import views as accounts_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/', v.register, name="register"),
-    path('', include('django.contrib.auth.urls')),
+    path('register/', accounts_views.register, name="register"),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name="login"),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name="logout"),
+    #path('', include('django.contrib.auth.urls')),
     path('', include('blog.urls')),   
 ]

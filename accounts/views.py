ba36e93a7633e6d3f38442667a2aa12b30ddc999
GBell94 <gmb1294@gmail.com>
2022-08-06 20:39:11 +0000

    Finish adding CRUD functionality to edit and delete posts

--- accounts/views.py
+++ accounts/views.py
@@ -1,19 +1,3 @@
-#from django.shortcuts import render, redirect
-#from .forms import RegisterForm
-
-# Create your views here.
-#def register(response):
-    #if response.method == "POST":
-        #form = RegisterForm(response.POST)
-        #if form.is_valid():
-            #form.save()
-            
-        #return redirect('/home')  
-    #else:   
-        #form = RegisterForm()
-
-    #return render(response, "register.html", {"form": form})
-
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
@@ -25,7 +9,7 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account has been created! You can now log in')
+            messages.success(request, f'{username}, your account has been created! You can now log in')
             return redirect('login')
     else:
         form = UserRegisterForm()
--- strengthfirst/urls.py
+++ strengthfirst/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('register/', accounts_views.register, name="register"),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name="login"),
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name="logout"),
-    #path('', include('django.contrib.auth.urls')),
+    #path('accounts/', include('django.contrib.auth.urls')),
     path('', include('blog.urls')),   
 ]
